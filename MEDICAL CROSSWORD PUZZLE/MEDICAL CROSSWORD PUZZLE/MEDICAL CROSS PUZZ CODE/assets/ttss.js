759eced99c1f2cd69df353fff984b1f6109beb50
RandiRodz <randi01187@gmail.com>
2025-05-09 19:32:41 -0500

    Code

---
+++ MEDICAL CROSSWORD PUZZLE/MEDICAL CROSSWORD PUZZLE/MEDICAL CROSS PUZZ CODE/assets/ttss.js
@@ -0,0 +1,39 @@
+var ttss = [
+	{
+		"word":"adhesion",
+		"clue":"Scars that connect two or more body structures together."
+	},
+	{
+		"word":"biopsy",
+		"clue":"The taking of a small sample of tissue for examination."
+	},
+	{
+		"word":"hemolysis",
+		"clue":"Breaking down of red blood cells in the body"
+	},
+	{
+		"word":"enzyme",
+		"clue":"A protein found in cells that speeds up chemical reactions in the body."
+	},
+	{
+		"word":"gene",
+		"clue":"A biological unit which passes on inherited information from parent to child, like facial characteristics"
+	},
+	{
+		"word":"colposcopy",
+		"clue":"A hospital examination used to diagnose, monitor, and treat cervical cell changes. "
+	},
+	{
+		"word":"diathermy",
+		"clue":"A surgical procedure to heat up and destroy body tissue or stop bleeding "
+	},
+	{
+		"word":"inflammation ",
+		"clue":"A bodily response in which white blood cells and other immune cell protect your body from disease or injury."
+	},
+	{
+		"word":"oedema",
+		"clue":"Swelling in any part of the body "
+	},
+	
+]
